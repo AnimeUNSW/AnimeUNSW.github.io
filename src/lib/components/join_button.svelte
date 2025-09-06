3b220b8387d081c6177caee4656d673e5de07253
Andy <andy.ruan2005@gmail.com>
2025-09-06 12:13:47 +1000

    add all missing dependencies and transfer over old code

---
+++ src/lib/components/join_button.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import down_arrow from "$lib/assets/arrow_drop_down.png"
+  import Button from "./ui/button/button.svelte";
+</script>
+
+<Button class="absolute top-6 right-9 bg-[#E880C8] text-2xl/[28px] text-black font-newcm">
+  Join
+  <img src={down_arrow} alt="">
+</Button>
