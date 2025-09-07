766ce79c1fa03ce8d9cdc7115a1ae4504b3f8936
Andy <andy.ruan2005@gmail.com>
2025-09-08 09:53:21 +1000

    create mobile components for sponsor cards

--- src/lib/components/sponsor_card.svelte
+++ src/lib/components/sponsor_card.svelte
@@ -3,7 +3,9 @@
 </script>
 
 
-<div class="flex w-[455px] h-[208px] justify-between p-[24px] rounded-[24px] bg-white">
+<!-- todo: fix hardcoded text and pictures later -->
+ <!-- desktop design -->
+<div class="flex w-[455px] h-[208px] justify-between p-[24px] rounded-[24px] gap-x-[24px] bg-white items-center hidden md:flex">
   <!-- column 1 -->
   <div class="flex flex-col w-[217px] h-[160px] gap-[16px]">
     <h3 class="text-[24px]/[28px]">Durin :emote:</h3>
@@ -11,8 +13,21 @@
   </div>
 
   <!-- column 2, image -->
-  <!-- image should be 172 by 172 px, 15 corner radius -->
   <div class="flex w-[172px] h-[172px] rounded-[15px] items-center">
+    <img class="w-full h-full object-cover" src={sampleImage} alt="">
+  </div>
+</div>
+
+<!-- mobile design -->
+<div class="flex w-[376px] h-[174px] justify-between p-[24px] rounded-[24px]  gap-x-[9px] bg-white items-center md:hidden">
+  <!-- column 1 -->
+  <div class="flex flex-col w-[189px] h-[124px] gap-[16px]">
+    <h3 class="text-[28px]/[28px]">Durin :emote:</h3>
+    <span class="break-words text-[16px]/[20px]">Use code DurinLetMedoUnspeakableThingsToYou at checkout for 99% off!</span>
+  </div>
+
+  <!-- column 2, image -->
+  <div class="flex w-[142px] h-[142px] rounded-[16px] items-center">
     <img class="w-full h-full object-cover" src={sampleImage} alt="">
   </div>
 </div>
--- src/routes/sponsors/+page.svelte
+++ src/routes/sponsors/+page.svelte
@@ -6,9 +6,10 @@
 
 <div class="bg-[#E9DEEC] min-h-screen">
   <Navbar pageName="Sponsors"></Navbar>
-  <!-- sponsor box -->
 
-  <div class="flex justify-center w-full">
+  <!-- todo: remove the need to create two separate divs for mobile and desktop later if possible -->
+  <!-- sponsor box (desktop)-->
+  <div class="flex justify-center w-full hidden md:flex">
     <div class="w-[934px] font-newcm flex flex-wrap gap-[24px]">
       <SponsorCard></SponsorCard>
       <SponsorCard></SponsorCard>
@@ -16,7 +17,17 @@
       <SponsorCard></SponsorCard>
       <SponsorCard></SponsorCard>
     </div>
-    
+  </div>
+
+  <!-- sponsor box mobile -->
+  <div class="flex justify-center w-full md:hidden">
+    <div class="w-[376px] font-newcm flex flex-wrap gap-y-[12px]">
+      <SponsorCard></SponsorCard>
+      <SponsorCard></SponsorCard>
+      <SponsorCard></SponsorCard>
+      <SponsorCard></SponsorCard>
+      <SponsorCard></SponsorCard>
+    </div>
   </div>
 
 </div>
