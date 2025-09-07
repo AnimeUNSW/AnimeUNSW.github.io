faa603c3cd1869e9ca657a4e4de60fe6bc9a2136
Andy <andy.ruan2005@gmail.com>
2025-09-07 19:30:02 +1000

    made template for desktop sponsors

---
+++ src/lib/components/sponsor_card.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import sampleImage from "../../lib/assets/discord.png"
+</script>
+
+
+<div class="flex w-[455px] h-[208px] justify-between p-[24px] rounded-[24px] bg-white">
+  <!-- column 1 -->
+  <div class="flex flex-col w-[217px] h-[160px] gap-[16px]">
+    <h3 class="text-[24px]/[28px]">Durin :emote:</h3>
+    <span class="break-words text-[20px]/[24px]">Use code DurinLetMedoUnspeakableThingsToYou at checkout for 99% off!</span>
+  </div>
+
+  <!-- column 2, image -->
+  <!-- image should be 172 by 172 px, 15 corner radius -->
+  <div class="flex w-[172px] h-[172px] rounded-[15px] items-center">
+    <img class="w-full h-full object-cover" src={sampleImage} alt="">
+  </div>
+</div>
--- src/routes/sponsors/+page.svelte
+++ src/routes/sponsors/+page.svelte
@@ -1,5 +1,25 @@
 <script>
   import Navbar from "../../lib/components/navbar.svelte"
+  import SponsorCard from "../../lib/components/sponsor_card.svelte";
 </script>
 
-<Navbar pageName="Sponsors"></Navbar>
+
+<div class="bg-[#E9DEEC] min-h-screen">
+  <Navbar pageName="Sponsors"></Navbar>
+  <!-- sponsor box -->
+
+  <div class="flex justify-center w-full">
+    <div class="w-[934px] font-newcm flex flex-wrap gap-[24px]">
+      <SponsorCard></SponsorCard>
+      <SponsorCard></SponsorCard>
+      <SponsorCard></SponsorCard>
+      <SponsorCard></SponsorCard>
+      <SponsorCard></SponsorCard>
+    </div>
+    
+  </div>
+
+</div>
+
+
+
