02c4f1217d05e4299cc1735ad8476c0ba06e275f
Andy <andy.ruan2005@gmail.com>
2025-09-08 15:17:08 +1000

    implement bottom frame of the info page

---
+++ src/lib/components/info_card.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+
+</script>
+
+<!-- card -->
+<div class="w-[425px] h-[290px] flex items-center justify-center rounded-[24px] bg-white">
+  <!-- content inside of the card -->
+  <div class="flex flex-col w-[369px] h-[234px] gap-y-[16px]">
+    <h3 class="text-[24px]/[28px]">Hard coded text</h3>
+    <p class="text-[20px]/[24px]">For all general questions and information, please don't hesitate to reach out. Simply send an email to real@true.yeah and we'll get back to you as soon as we can. Alternatively, feel free to reach out on any of our socials, which can be found in the top right corner of the website under the “Join” header.</p>
+  </div>
+</div>
