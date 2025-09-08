becaae0b9681e25289374c1cd49ecd47adc11f11
Andy <andy.ruan2005@gmail.com>
2025-09-08 15:38:18 +1000

    create layout info page

--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -5,12 +5,19 @@
 
 <div class="font-newcm bg-pink-100">
   <Navbar pageName="Info"></Navbar>
-  <!-- create a contact box -->
-  <div class="flex flex-col w-[822px] h-388px] gap-y-[70px] justify-center">
-    <h2 class="text-[36px]/[28px] text-center">Want to contact us?</h2>
-    <div class="flex gap-x-[32px]">
-      <InfoCard></InfoCard>
-      <InfoCard></InfoCard>
+  <div class="flex flex-col gap-y-[64px]">
+    <div>
+      Accordion frame
+    </div>
+
+    <!-- contact box frame 2 -->
+    <div class="flex flex-col w-[822px] h-388px] gap-y-[70px] justify-center">
+      <h2 class="text-[36px]/[28px] text-center">Want to contact us?</h2>
+      <div class="flex gap-x-[32px]">
+        <InfoCard></InfoCard>
+        <InfoCard></InfoCard>
+      </div>
     </div>
   </div>
+
 </div>
