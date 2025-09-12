75fc53038447377e19378b09c13eb13e2a246634
Andy <andy.ruan2005@gmail.com>
2025-09-12 13:59:28 +1000

    create event page

---
+++ src/lib/components/events/event-card.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import durin from "$lib/assets/durin.svg"
+  let {largeImage, line} = $props();
+</script>
+
+<div class="flex flex-col w-[453px] p-4 gap-y-[12px] bg-white rounded-3xl flex-grow-0">
+  <!-- image -->
+  <div class={`w-[421px] ${largeImage ? 'h-[281px]' : 'h-[237px]'}`}>
+    <img src={durin} alt="" class="w-full h-full object-cover rounded-lg">
+  </div>
+  <p class="w-[421px] text-xl/6 p-1">{line}</p>
+</div>
---
+++ src/routes/events/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import EventCard from "$lib/components/events/event-card.svelte";
+
+  const shortText = "AUNSW member in the wild"
+
+  const longText = shortText + " but longer text real wow I love multiple lines mhmh true and real"
+</script>
+
+<!-- outer div -->
+<div class="flex mt-8 justify-center">
+  <div class="flex flex-wrap w-[930px] gap-6 items-start">
+    <EventCard largeImage={false} line={shortText}/>
+    <EventCard largeImage={true} line={longText}/>
+    <EventCard largeImage={true} line={longText}/>
+    <EventCard largeImage={true} line={longText}/>
+  </div>
+</div>
