a8fd5d96620c745cc064bfc80a5b833dc497b0db
Andy <andy.ruan2005@gmail.com>
2025-09-12 23:21:48 +1000

    fix sizing of image in event card

--- src/lib/components/events/event-card.svelte
+++ src/lib/components/events/event-card.svelte
@@ -5,7 +5,7 @@
 
 <div class="flex flex-col md:w-[453px] w-[376px] p-4 gap-y-3 bg-white rounded-3xl">
   <!-- image -->
-  <div class={`md:w-[421px] w-[352px] h-[198px] ${largeImage ? 'md:h-[281px]' : 'md:h-[237px]'}`}>
+  <div class={`w-full h-[198px] ${largeImage ? 'md:h-[281px]' : 'md:h-[237px]'}`}>
     <img src={durin} alt="" class="w-full h-full object-cover rounded-lg">
   </div>
   <p class="md:w-[421px] w-[352px] md:text-xl/6 text-base/5 p-1">{line}</p>
