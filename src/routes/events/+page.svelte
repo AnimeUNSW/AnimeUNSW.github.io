79a08a65ed91746614615203f6c0dcc5c340f68a
Andy <andy.ruan2005@gmail.com>
2025-09-12 23:26:42 +1000

    add a todo in events page

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
   import EventCard from "$lib/components/events/event-card.svelte";
-
   const shortText = "AUNSW member in the wild"
-
   const longText = shortText + " but longer text real wow I love multiple lines mhmh true and real"
 </script>
 
 <!-- outer div -->
 <div class="flex md:mt-8 mt-[14px] justify-center">
   <div class="flex flex-wrap md:w-[930px] w-[376px] md:gap-6 gap-3 items-start">
+    <!-- had to use 2 separate columns because the sizing of the cards would get stretched -->
+    <!-- todo: find a way to not use 2 separate divs? -->
     <!-- column 1 -->
     <div class="flex flex-col md:gap-6 gap-3">
       <EventCard largeImage={false} line={shortText}/>
