00f9f5d13fb1ffb7052d6559b2650b41485a7b5b
Andy <andy.ruan2005@gmail.com>
2025-09-17 20:07:08 +1000

    add social cards to home page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { HomeSocialCard } from "$lib/components/ui/card";
+</script>
+
+<HomeSocialCard icon="discord" numberFollowers="3200+ Members"/>
+<HomeSocialCard icon="instagram" numberFollowers="2200+ Followers"/>
+<HomeSocialCard icon="facebook" numberFollowers="1700+ Followers"/>
+<HomeSocialCard icon="xiaohongshu" numberFollowers="10+ Followers"/>
