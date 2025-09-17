08248e65b540d1cb9ac8e1312810c1c073c32c19
Andy <andy.ruan2005@gmail.com>
2025-09-17 20:01:30 +1000

    write code for social card

--- src/lib/components/ui/card/index.ts
+++ src/lib/components/ui/card/index.ts
@@ -1,9 +1,12 @@
 import EventCard from "./event-card.svelte";
 import SponsorCard from "./sponsor-card.svelte";
 import InfoCard from "./info-card.svelte";
+import HomeSocialCard from "./home-social-card.svelte";
+
 export {
   //
   EventCard,
   InfoCard,
   SponsorCard,
+  HomeSocialCard
 };
