ef3ab4296ae550d012443c65ba421cbdc1ea47bf
Rin <31785428+rin-senpai@users.noreply.github.com>
2025-09-23 22:01:06 +1000

    increased nav-item hover opacity

--- src/lib/components/ui/nav/nav-item.svelte
+++ src/lib/components/ui/nav/nav-item.svelte
@@ -12,8 +12,10 @@
       "absolute -inset-1 -top-2 -right-4 -bottom-[6px] -left-4 inline-block rounded-full transition-all group-hover:text-white",
       page.url.pathname === href
         ? "opacity-100"
-        : "opacity-0 group-hover:opacity-5",
-      page.url.pathname === "/" && scrollY <= 100 ? "bg-black" : "bg-secondary",
+        : "opacity-0 group-hover:opacity-8 dark:group-hover:opacity-15",
+      page.url.pathname === "/" && scrollY <= 100
+        ? "bg-white group-hover:opacity-15"
+        : "bg-secondary",
     ]}
     aria-hidden="true"
   ></span>
