a0c170d91da325899098d369369b1ff4497c50c3
りん <31785428+rin-senpai@users.noreply.github.com>
2025-09-25 21:31:48 +1000

    added all events button

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { EventCard } from "$lib/components/ui/card";
+  import { Button } from "$lib/components/ui/button";
   const eventImageModules = import.meta.glob(
     "$lib/assets/events/*.{png,jpg,jpeg}",
     {
@@ -34,7 +35,7 @@
 </svelte:head>
 
 <div
-  class="mt-3 mb-8 flex w-full items-center justify-center px-2 md:mt-8 md:px-8"
+  class="mt-3 mb-4 flex w-full items-center justify-center px-2 md:mt-8 md:px-8"
 >
   <div
     class="w-full columns-1 gap-x-2 text-left sm:columns-2 md:gap-x-6 xl:columns-3 2xl:w-[1472px]"
@@ -50,4 +51,12 @@
       {/each}
     {/if}
   </div>
+</div>
+<div class="flex items-center justify-center pb-8">
+  <a
+    href="https://campus.hellorubric.com/?s=12432"
+    target="_blank"
+    class="bg-primary hover:bg-primary/85 rounded-3xl px-4 py-2 text-xl transition-all hover:rounded-xl"
+    >All Events</a
+  >
 </div>
